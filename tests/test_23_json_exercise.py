51e1f591a2ae7b1a7037ac1670d11849bf6490c2
joeeoj <joeeoj@blueskyemail.net>
2021-04-26 19:14:54 -0700

    change to ex 23 tests to remove indeterminism

--- tests/test_23_json_exercise.py
+++ tests/test_23_json_exercise.py
@@ -10,10 +10,6 @@ DATA_DIR = Path(__file__).parent / 'test_data' / 'scores'
 
 def test_files(capfd):
     expected = f"""\
-{DATA_DIR / '9b.json'}
-    math min: 70, max: 100, average: 86.2
-    literature min: 60, max: 99, average: 78.8
-    science min: 72, max: 97, average: 89.0
 {DATA_DIR / '9a.json'}
     math min: 65, max: 100, average: 85.0
     literature min: 78, max: 98, average: 83.6
